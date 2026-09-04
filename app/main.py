f2962992a44bb79338da8456e27d4d7111371ad4
sheyda vahidi <sheeydaa84@gmail.com>
2026-09-04 20:58:32 +0330

    sorting liberies

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import Session
-from pydantic import BaseModel
 
 from app.database import Base, engine, get_db
 
